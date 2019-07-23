8de13247f4e6d22c61138a60ad6c29329189ce88
mixerekt <ant1058@yandex.ru>
2019-07-23 16:42:21 +0500

    simple maven ci builder

--- src/main/groovy/job_maven_builder.groovy
+++ src/main/groovy/job_maven_builder.groovy
@@ -1,9 +1,33 @@
 import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.Job
 
-class builder{
+class MavenJobBuilder {
+    def jobName
+    def description
+    def gitUrl
+    def branchName
+    def credentialsId
+    def numToKeep
+    def daysToKeep
 
-    Job build(DslFactory dslFactory){
-
+    Job build(DslFactory dslFactory) {
+        dslFactory.mavenJob(jobName) {
+            description(description)
+            logRotator {
+                numToKeep = this.numToKeep
+                daysToKeep = this.daysToKeep
+            }
+            scm {
+                git {
+                    remote {
+                        name("origin")
+                        url(gitUrl)
+                        credentialsId(this.credentialsId)
+                    }
+                    branch(branchName)
+                }
+            }
+            goals('clean package')
+        }
     }
 }
