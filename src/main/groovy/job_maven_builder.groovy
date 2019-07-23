912004babbfff3b312e26df03b9b53aa97eab159
mixerekt <ant1058@yandex.ru>
2019-07-23 17:32:04 +0500

    fix: mavenJob in folder

--- src/main/groovy/job_maven_builder.groovy
+++ src/main/groovy/job_maven_builder.groovy
@@ -34,14 +34,13 @@ class MavenJobBuilder {
 
 folder('git-maven-folder') {
     description('This is folder for maven builder')
+    def builder = new MavenJobBuilder(jobName: "Maven Job",
+            description: "simple maven job builder",
+            gitUrl: "https://github.com/mixerekt/testing-job-repo.git",
+            branchName: "master",
+            credentialsId: "c69356b9-e9d8-46d6-b2d6-0fe73f49d2e4",
+            numToKeep: 10,
+            daysToKeep: 20)
+
+    builder.build(this)
 }
-
-def builder = new MavenJobBuilder(jobName: "Maven Job",
-        description: "simple maven job builder",
-        gitUrl: "https://github.com/mixerekt/testing-job-repo.git",
-        branchName: "master",
-        credentialsId: "c69356b9-e9d8-46d6-b2d6-0fe73f49d2e4",
-        numToKeep: 10,
-        daysToKeep: 20)
-
-builder.build(this)
